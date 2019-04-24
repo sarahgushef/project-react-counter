c888f46fb6b74797fbfe256c9190bb152b6facbd
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-04-24 18:26:21 +0700

    delete unused code

--- src/App.js
+++ src/App.js
@@ -2,27 +2,6 @@ import React from "react"
 
 import "./App.css"
 
-// function App() {
-//   let value = 0
-
-//   const reduceValue = () => {
-//     return value - 1
-//   }
-
-//   return (
-//     <div className="App">
-//       <span>{value}</span>
-
-//       <div>
-//         <button onClick="reduceValue()">-</button>
-//         <button onClick="addValue()">+</button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
 export default class App extends React.Component {
   constructor() {
     super()
@@ -54,7 +33,7 @@ export default class App extends React.Component {
           <button onClick={this.addValue}>+</button>
         </div>
 
-        <p>CSSnya nanti dulu ya hehehehe</p>
+        <p>CSSnya nanti dulu ya hehehehe </p>
       </div>
     )
   }
